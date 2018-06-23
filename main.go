afb473fbb4df284858b13d2c3161533e7c348109
Mohanarangan Muthukumar <mohan@extrasalt.org>
2018-06-23 11:34:51 +0530

    Initialize vuejs

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("app")))
-	fmt.Println("Hello world")
+	fmt.Println("Running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
