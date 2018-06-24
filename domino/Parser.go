4a6f15ea34ea92ac78422fe9754c8d0fb11ee05d
Shreya <c.shreya2204@gmail.com>
2018-06-24 07:28:29 +0530

    Fix tag for aws instance parsing

--- domino/Parser.go
+++ domino/Parser.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hashicorp/terraform/terraform"
-	"os"
-	"strings"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/hashicorp/terraform/terraform"
 )
 
 type Plans []struct {
-    EphemeralDisks  string `json:"ephemeral_disks"`
-    MemoryInGb      float64 `json:"memory_in_gb"`
-    Name            string `json:"name"`
-    ParsedTimestamp int64  `json:"parsed_timestamp"`
-    PricePerHour    float64 `json:"price_per_hour"`
-    Provider        string `json:"provider"`
-    Region          string `json:"region"`
-    Vcpus           int64 `json:"vcpus"`
+	EphemeralDisks  string  `json:"ephemeral_disks"`
+	MemoryInGb      float64 `json:"memory_in_gb"`
+	Name            string  `json:"name"`
+	ParsedTimestamp int64   `json:"parsed_timestamp"`
+	PricePerHour    float64 `json:"price_per_hour"`
+	Provider        string  `json:"provider"`
+	Region          string  `json:"region"`
+	Vcpus           int64   `json:"vcpus"`
 }
 
 var priceMap map[string]float64
 
 func main() {
-	
-	if(len(os.Args[1:]) == 0) {
+
+	if len(os.Args[1:]) == 0 {
 		fmt.Println("Usage: domino [path-to-terraform-plan-file]\nThe above command will take in your terraform plan and display the total cost according to current CSP pricing.")
 		os.Exit(0)
 	}
-	
+
 	terraformPlanFile := os.Args[1]
 
-	res, _ := http.Get("http://localhost:8000/azure_vm_prices.json")
+	res, _ := http.Get("http://localhost:8000/aws_ec2_prices.json")
 	temp, _ := ioutil.ReadAll(res.Body)
 
 	var plans Plans
@@ -45,15 +46,15 @@ func main() {
 	processTerraformPlan(terraformPlanFile)
 }
 
-func planToPriceMap(plans Plans) (map[string]float64) {
+func planToPriceMap(plans Plans) map[string]float64 {
 	priceMap := make(map[string]float64)
-	for _, plan:= range plans {
+	for _, plan := range plans {
 		priceMap[strings.ToLower(plan.Name)] = plan.PricePerHour
 	}
 	return priceMap
 }
 
-func processTerraformPlan(planFile string){
+func processTerraformPlan(planFile string) {
 	file, err := os.Open(planFile)
 	if err != nil {
 		fmt.Println(err)
@@ -67,24 +68,28 @@ func processTerraformPlan(planFile string){
 	for moduleIdx := range plan.Diff.Modules {
 		var instanceType string
 		var sum float64
-		
+
 		for resource, instanceDiff := range plan.Diff.Modules[moduleIdx].Resources {
 			resourceType := strings.Split(resource, ".")[0]
 			switch resourceType {
-				case "aws_instance":
-					instanceType = instanceDiff.Attributes["aws_instance_type"].New
+			case "aws_instance":
+				instanceType = instanceDiff.Attributes["instance_type"].New
+				fmt.Println("resource type recognized: ", instanceType)
 
-				case "azurerm_virtual_machine":
-					instanceType = instanceDiff.Attributes["vm_size"].New
+			case "azurerm_virtual_machine":
+				instanceType = instanceDiff.Attributes["vm_size"].New
+				fmt.Println("resource type recognized: ", instanceType)
 
-				case "digitalocean_droplet":
-					instanceType = instanceDiff.Attributes["size"].New
-				
-				case "google_compute_instance":
-					instanceType = instanceDiff.Attributes["machine_type"].New	
+			case "digitalocean_droplet":
+				instanceType = instanceDiff.Attributes["size"].New
+				fmt.Println("resource type recognized: ", instanceType)
 
-				default:
-					fmt.Println("resource type not recognized: ", resourceType)
+			case "google_compute_instance":
+				instanceType = instanceDiff.Attributes["machine_type"].New
+				fmt.Println("resource type recognized: ", instanceType)
+
+			default:
+				fmt.Println("resource type not recognized: ", resourceType)
 			}
 			sum = sum + calculateCost(instanceType)
 		}
@@ -92,6 +97,6 @@ func processTerraformPlan(planFile string){
 	}
 }
 
-func calculateCost(instanceType string) (float64){
+func calculateCost(instanceType string) float64 {
 	return priceMap[strings.ToLower(instanceType)]
 }
