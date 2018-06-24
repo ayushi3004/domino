46a0a6e893e52f579a17e4a10aaed1e5e1d55fda
Shreya <c.shreya2204@gmail.com>
2018-06-24 08:23:49 +0530

    Make mapping for memory, cpus and prices within the same map

--- domino/Parser.go
+++ domino/Parser.go
@@ -33,25 +33,84 @@ func main() {
 
 	terraformPlanFile := os.Args[1]
 
-	res, _ := http.Get("http://localhost:8000/aws_ec2_prices.json")
-	temp, _ := ioutil.ReadAll(res.Body)
+	res_aws, _ := http.Get("http://localhost:8000/aws_ec2_prices.json")
+	res_azure, _ := http.Get("http://localhost:8000/azure_vm_prices.json")
+	res_do, _ := http.Get("http://localhost:8000/do_droplets_prices.json")
+	res_gce, _ := http.Get("http://localhost:8000/gce_prices.json")
 
-	var plans Plans
-	err := json.Unmarshal(temp, &plans)
-	if err != nil {
-		fmt.Println("There was an error:", err)
+	temp_aws, _ := ioutil.ReadAll(res_aws.Body)
+	temp_azure, _ := ioutil.ReadAll(res_azure.Body)
+	temp_do, _ := ioutil.ReadAll(res_do.Body)
+	temp_gce, _ := ioutil.ReadAll(res_gce.Body)
+
+	var plans_aws Plans
+	var plans_azure Plans
+	var plans_do Plans
+	var plans_gce Plans
+
+	err1 := json.Unmarshal(temp_aws, &plans_aws)
+	if err1 != nil {
+		fmt.Println("There was an error:", err1)
 	}
-	priceMap = planToPriceMap(plans)
+
+	err2 := json.Unmarshal(temp_azure, &plans_azure)
+	if err2 != nil {
+		fmt.Println("There was an error:", err2)
+	}
+	err3 := json.Unmarshal(temp_do, &plans_do)
+	if err3 != nil {
+		fmt.Println("There was an error:", err3)
+	}
+
+	err4 := json.Unmarshal(temp_gce, &plans_gce)
+	if err4 != nil {
+		fmt.Println("There was an error:", err4)
+	}
+
+	allPlans := []Plans{}
+	allPlans = append(allPlans, plans_aws)
+	allPlans = append(allPlans, plans_azure)
+	allPlans = append(allPlans, plans_do)
+	allPlans = append(allPlans, plans_gce)
+
+	priceMap = planToPriceMap(allPlans)
+
+	// fmt.Println(priceMap)
 
 	processTerraformPlan(terraformPlanFile)
+
+	// suggestInstances(terraformPlanFile)
+
 }
 
-func planToPriceMap(plans Plans) map[string]float64 {
+func planToPriceMap(allPlans []Plans) map[string]float64 {
 	priceMap := make(map[string]float64)
-	for _, plan := range plans {
-		priceMap[strings.ToLower(plan.Name)] = plan.PricePerHour
+	for _, plans := range allPlans {
+		for _, plan := range plans {
+			priceMap[strings.ToLower(plan.Name)] = plan.PricePerHour
+		}
 	}
 	return priceMap
+}
+
+func planToMemoryMap(allPlans []Plans) map[string]float64 {
+	memoryMap := make(map[string]float64)
+	for _, plans := range allPlans {
+		for _, plan := range plans {
+			memoryMap[strings.ToLower(plan.Name)] = plan.MemoryInGb
+		}
+	}
+	return memoryMap
+}
+
+func planToCPUMap(allPlans []Plans) map[string]int64 {
+	cpuMap := make(map[string]int64)
+	for _, plans := range allPlans {
+		for _, plan := range plans {
+			cpuMap[strings.ToLower(plan.Name)] = plan.Vcpus
+		}
+	}
+	return cpuMap
 }
 
 func processTerraformPlan(planFile string) {
@@ -99,4 +158,24 @@ func processTerraformPlan(planFile string) {
 
 func calculateCost(instanceType string) float64 {
 	return priceMap[strings.ToLower(instanceType)]
+}
+
+func suggestInstances(planFile string) {
+	file, err := os.Open(planFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	plan, err := terraform.ReadPlan(file)
+	if err != nil {
+		panic(err)
+	}
+
+	for moduleIdx := range plan.Diff.Modules {
+
+		for _, instanceDiff := range plan.Diff.Modules[moduleIdx].Resources {
+			// resourceType := strings.Split(resource, ".")[0]
+			fmt.Println(instanceDiff.Attributes)
+		}
+	}
 }
